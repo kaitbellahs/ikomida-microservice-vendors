8900682a776f92958037874bd36253df3f5e697f
khalid ait bellahs <khalid@tialtonivel.com.br>
2022-12-04 10:14:30 -0300

    EMail bugs fix

--- src/controllers/App.ts
+++ src/controllers/App.ts
@@ -1,115 +1,115 @@
 import { Domain, Utils, DBModels, Types } from '@ikomida/shared-backend'
 
 export default class App {
-    logger
-    googleAdmin
+  logger
+  googleAdmin
 
-    constructor(logger: Utils.Logger) {
-        this.logger = logger
-        this.googleAdmin = new Utils.GoogleAdmin(this.logger)
+  constructor(logger: Utils.Logger) {
+    this.logger = logger
+    this.googleAdmin = new Utils.GoogleAdmin(this.logger)
+  }
+
+  async getApp(identity: Types.Classes.CUser) {
+    const contractModel = await DBModels.ContractModel.findOne({
+      where: {
+        ikomidaID: identity.ikomidaID
+      },
+      include: [
+        {
+          model: DBModels.AppModel,
+          required: false
+        }
+      ]
+    })
+    if (!contractModel) {
+      const error = new Utils.iKomidaError(Utils.iKomidaError.IKOMIDA_VENDOR_SERVICE_GET_LAYOUTS_INVALID_CONTRACT)
+      return error.logAndReturn(this.logger)
+    }
+    const appsModel = contractModel?.apps
+    if (!appsModel) {
+      const error = new Utils.iKomidaError(Utils.iKomidaError.IKOMIDA_VENDOR_SERVICE_GET_LAYOUTS_EMPTY)
+      return error.logAndReturn(this.logger)
     }
 
-    async getApp(identity: Types.Classes.CUser,) {
-        const contractModel = await DBModels.ContractModel.findOne({
-            where: {
-                ikomidaID: identity.ikomidaID
-            },
-            include: [
-                {
-                    model: DBModels.AppModel,
-                    required: false
-                }
-            ]
-        })
-        if (!contractModel) {
-            const error = new Utils.iKomidaError(Utils.iKomidaError.IKOMIDA_VENDOR_SERVICE_GET_LAYOUTS_INVALID_CONTRACT)
-            return error.logAndReturn(this.logger)
-        }
-        const appsModel = contractModel?.apps
-        if (!appsModel) {
-            const error = new Utils.iKomidaError(Utils.iKomidaError.IKOMIDA_VENDOR_SERVICE_GET_LAYOUTS_EMPTY)
-            return error.logAndReturn(this.logger)
-        }
+    const apps: Types.Classes.CApp[] = appsModel.map(appModel => {
+      return Types.Classes.CApp.init(
+        appModel.bundleId ?? '',
+        appModel.displayName ?? '',
+        appModel.platform ?? '-',
+        undefined,
+        appModel.version,
+        undefined,
+        appModel.storePublishStatus,
+        appModel.active,
+        undefined,
+        undefined,
+        undefined,
+        undefined,
+        appModel.storeVersion,
+        undefined,
+        undefined,
+        appModel.icon,
+        appModel.description,
+        appModel.androidLink,
+        appModel.iosLink,
+        appModel.id
+      )
+    })
 
-        const apps: Types.Classes.CApp[] = appsModel.map(appModel => {
-            return Types.Classes.CApp.init(
-                appModel.bundleId ?? '',
-                appModel.displayName ?? '',
-                appModel.platform ?? '-',
-                undefined,
-                appModel.version,
-                undefined,
-                appModel.storePublishStatus,
-                appModel.active,
-                undefined,
-                undefined,
-                undefined,
-                undefined,
-                appModel.storeVersion,
-                undefined,
-                undefined,
-                appModel.icon,
-                appModel.description,
-                appModel.androidLink,
-                appModel.iosLink,
-                appModel.id
-            )
-        })
+    return new Utils.Return(true, apps)
+  }
 
-        return new Utils.Return(true, apps)
+  async updateApp(identity: Types.Classes.CUser, input: any) {
+    try {
+      const payload: Types.Classes.CApp[] = Types.Classes.CApp.fromObject(input)
+      if (payload.length === 0) {
+        const error = new Utils.iKomidaError(Utils.iKomidaError.IKOMIDA_VENDOR_SERVICE_SET_LAYOUTS_MISSING_DATA)
+        return error.logAndReturn(this.logger)
+      }
+      const app = payload.filter(app => {
+        return app.icon || app.description
+      })?.[0]
+      if (!app) {
+        const error = new Utils.iKomidaError(Utils.iKomidaError.IKOMIDA_VENDOR_SERVICE_SET_LAYOUTS_MISSING_DATA)
+        return error.logAndReturn(this.logger)
+      }
+      const contractModel = await DBModels.ContractModel.findOne({
+        where: {
+          ikomidaID: identity.ikomidaID
+        },
+        include: [
+          {
+            model: DBModels.AppModel,
+            required: true
+          }
+        ]
+      })
+      if (!contractModel) {
+        const error = new Utils.iKomidaError(Utils.iKomidaError.IKOMIDA_VENDOR_SERVICE_SET_LAYOUT_INVALID_CONTRACT)
+        return error.logAndReturn(this.logger)
+      }
+      const appsModel = contractModel?.apps
+      if (!appsModel) {
+        const error = new Utils.iKomidaError(Utils.iKomidaError.IKOMIDA_VENDOR_SERVICE_SET_LAYOUT_EMPTY)
+        return error.logAndReturn(this.logger)
+      }
+      const icon = await this.googleAdmin.uploadToStorage(
+        identity,
+        'appIcon',
+        'image',
+        'vendorAppIcon',
+        app.icon,
+        app.icon
+      )
+      for (const appModel of appsModel) {
+        appModel.icon = icon
+        appModel.description = app.description
+        await appModel.save()
+      }
+      return new Utils.Return(true, null)
+    } catch (exception: any) {
+      const error = new Utils.iKomidaError(Utils.iKomidaError.IKOMIDA_VENDOR_SERVICE_SET_LAYOUT_EXCEPTION, exception)
+      return error.logAndReturn(this.logger)
     }
-
-    async updateApp(identity: Types.Classes.CUser, input: any) {
-        try {
-            const payload: Types.Classes.CApp[] = Types.Classes.CApp.fromObject(input)
-            if (payload.length === 0) {
-                const error = new Utils.iKomidaError(Utils.iKomidaError.IKOMIDA_VENDOR_SERVICE_SET_LAYOUTS_MISSING_DATA)
-                return error.logAndReturn(this.logger)
-            }
-            const app = payload.filter(app => {
-                return app.icon || app.description
-            })?.[0]
-            if (!app) {
-                const error = new Utils.iKomidaError(Utils.iKomidaError.IKOMIDA_VENDOR_SERVICE_SET_LAYOUTS_MISSING_DATA)
-                return error.logAndReturn(this.logger)
-            }
-            const contractModel = await DBModels.ContractModel.findOne({
-                where: {
-                    ikomidaID: identity.ikomidaID
-                },
-                include: [
-                    {
-                        model: DBModels.AppModel,
-                        required: true
-                    }
-                ]
-            })
-            if (!contractModel) {
-                const error = new Utils.iKomidaError(Utils.iKomidaError.IKOMIDA_VENDOR_SERVICE_SET_LAYOUT_INVALID_CONTRACT)
-                return error.logAndReturn(this.logger)
-            }
-            const appsModel = contractModel?.apps
-            if (!appsModel) {
-                const error = new Utils.iKomidaError(Utils.iKomidaError.IKOMIDA_VENDOR_SERVICE_SET_LAYOUT_EMPTY)
-                return error.logAndReturn(this.logger)
-            }
-            const icon = await this.googleAdmin.uploadToStorage(
-                identity,
-                'appIcon',
-                'image',
-                'vendorAppIcon',
-                app.icon,
-                app.icon
-            )
-            for (const appModel of appsModel) {
-                appModel.icon = icon
-                appModel.description = app.description
-                await appModel.save()
-            }
-            return new Utils.Return(true, null)
-        } catch (exception: any) {
-            const error = new Utils.iKomidaError(Utils.iKomidaError.IKOMIDA_VENDOR_SERVICE_SET_LAYOUT_EXCEPTION, exception)
-            return error.logAndReturn(this.logger)
-        }
-    }
+  }
 }
